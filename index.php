3112c95610f4f96808c80ce35e8c946ad2a5ef3a
nicola1397 <nicola1397@outlook.it>
2024-02-26 19:57:33 +0100

    first commit ( not working )

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
+<?php require_once "./result.php" ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>HTML</title>
+    <title>PHP HOTEL MAIN</title>
+
+    <!-- LINK BOOTSTRAP -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<!-- SCRIPT BOOTSRAP -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
   </head>
   <body>
-    <form action="./result.php" method="POST">
+    
+    <form action="./index.php" method="GET">
+      <div>
     <label for="star-select">Stelle:</label>
 
 <select name="stars" id="star-select">
@@ -14,9 +24,42 @@
   <option value="2+">2+ stelle</option>
   <option value="3+">3+ stelle</option>
   <option value="4+">4+ stelle</option>
-</select>
+</select></div>
+<div>
+<label for="parking">Parcheggio:</label>
+<input type="radio" name="parking" id="yes" value="yes">Si</div>
+<input type="radio" name="parking" id="no" value="no">No</div>
+<input type="radio" name="parking" id="all" value="all">Mostra tutti</div>
 
+</div>
       <button type="submit">INVIA</button>
     </form>
+
+<div>
+<table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Nome</th>
+          <th scope="col">Descrizione</th>
+          <th scope="col">Parcheggio</th>
+          <th scope="col">Voto</th>
+          <th scope="col">Distanza dal centro</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <?php foreach ($filter_hotels as $hotel) : ?>
+          <tr>
+            <td><?= $hotel["name"] ?></td>
+            <td><?= $hotel["description"] ?></td>
+            <td><?= $hotel["parking"] ? 'true' : 'false' ?></td>
+            <td><?= $hotel["vote"] ?></td>
+            <td><?= $hotel["distance_to_center"] . " km" ?></td>
+          </tr>
+        <?php endforeach ?>
+      </tbody>
+    </table>
+</div>
+   
   </body>
 </html>
--- result.php
+++ result.php
@@ -1,56 +1,69 @@
 <?php
 
-    $hotels = [
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+$filter_hotels = [];
 
-    ];
+$filter_selected = !empty($_GET);
 
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP HOTEL</title>
-</head>
-<body>
-    <?php ?>
-    
-</body>
-</html>
+
+if ($filter_selected) {
+    $parking_filter = $_GET["parking"];
+
+    if ($parking_filter == "si") {
+        $filter_value = true;
+    } elseif ($parking_filter == "no") {
+        $filter_value = false;
+    } else {
+        $filter_value = "all";
+    };
+
+    $vote_filter = $_GET["stars"];
+
+    foreach ($hotels as $hotel) {
+        if ($hotel["parking"] === $filter_value && $hotel["vote"] >= $vote_filter || $hotel["parking"] === $filter_value && $vote_filter == "all" || $filter_value == "all" && $hotel["vote"] >= $vote_filter) {
+            $filter_hotels[] = $hotel;
+        } elseif ($parking_filter == "all" && $vote_filter == "all") {
+            $filter_hotels = $hotels;
+        }
+    };
+} else {
+    $filter_hotels = $hotels;
+};
