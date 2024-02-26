70f0861d32cb07f42c1c9a6f6a05544a713f4b8d
nicola1397 <nicola1397@outlook.it>
2024-02-26 19:40:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>HTML</title>
+  </head>
+  <body>
+    <form action="./result.php" method="POST">
+    <label for="star-select">Stelle:</label>
+
+<select name="stars" id="star-select">
+  <option value="">Scegli un'opzione</option>
+  <option value="2+">2+ stelle</option>
+  <option value="3+">3+ stelle</option>
+  <option value="4+">4+ stelle</option>
+</select>
+
+      <button type="submit">INVIA</button>
+    </form>
+  </body>
+</html>
